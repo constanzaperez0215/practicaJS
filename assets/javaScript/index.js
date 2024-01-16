23dd9ae5d1fb91a69547ad80a30e577f09c2a59a
Constanza <constanza.vp.o@gmail.com>
2024-01-16 13:59:15 -0300

    JS INPUT password secreto

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -3,7 +3,9 @@
 // if (temperatura > 30)
 // alert ("uff que calor")
 
-password ="secreto"
-if (password == "secreto") {
-    alert("conoces el password?")
-}
+// password ="secreto"
+// if (password == "secreto") {
+//     alert("conoces el password?")
+// }
+
+
